ad9c170ab6fdf174f2621fa539e2411ad9318fcf
gerard sychay <hellogerard@gmail.com>
2010-05-05 23:50:01 -0400

    optimize.css task

--- phing/filters/CSSOptimizer.php
+++ phing/filters/CSSOptimizer.php
@@ -9,21 +9,24 @@
  */
 
 
-require_once('class.yui_compressor.php');
-
-
 class CSSOptimizer
 {
-    private $_staticWebRoot;
-    private $_cssDir;
+    private $_host;
+    private $_yuiPath;
+    private $_webRoot;
+    private $_toDir;
+    private $_phing;
 
-    public function __construct($staticWebRoot, $cssDir)
+    public function __construct($host, $yuiPath, $webRoot, $toDir, $phing)
     {
-        $this->_staticWebRoot = $staticWebRoot;
-        $this->_cssDir = $cssDir;
+        $this->_host = $host;
+        $this->_yuiPath = $yuiPath;
+        $this->_webRoot = $webRoot;
+        $this->_toDir = $toDir;
+        $this->_phing = $phing;
     }
 
-    public function build($buffer)
+    public function optimize($buffer)
     {
         $output = '';
 
@@ -34,172 +37,121 @@ class CSSOptimizer
         $dom->loadHTML($buffer);
         $html = simplexml_import_dom($dom);
 
-        // gather file names
+        // gather file names -
         // get URL's for each CSS file in each CSS tag of the HTML block
-        $urls = array();
+        $files = array();
         foreach ($html->head->link as $link)
         {
             // filter out things like favicon
             if ((string) $link['rel'] == 'stylesheet')
             {
                 $file = trim((string) $link['href']);
-                $urls[] = $file;
+                $files[] = trim($file, '/');
             }
         }
 
         // set up some variables for str_replace()
         $needles = array('/', '.css');
-        $replace = array('_', '');
+        $replace = array('.', '');
 
-        // get list of all partners for expansion
-        $partners = @scandir($this->_cssDir . "/partners");
-        array_unshift($partners, 'core');
+        // get the final filename
+        $bundlefile = str_replace($needles, $replace, join('-', $files));
 
-        // for each partner, create a combined, minified file
-        foreach ($partners as $partner)
+        // build the combined, minified output file
+        // the latest mod time of the set is in output file name
+        try
         {
-            if ($partner == "." || $partner == "..")
-            {
-                continue;
-            }
+            $this->_phing->log("Building {$this->_toDir}/$bundlefile.css", Project::MSG_VERBOSE);
 
-            if (($files = $this->_determineSet($urls, $latest, $partner)) !== false)
-            {
-                // get the final filename
-                $set = str_replace($needles, $replace, implode('-', $files));
-
-                if (($newLatest = $this->_compareSet($set, $latest)) !== false)
-                {
-                    // build the output file
-                    // the latest mod time of the set is in output file name
-                    try
-                    {
-                        $this->_saveSet($set, $files, $newLatest);
-                    }
-                    catch (Exception $e)
-                    {
-                        $this->log($e->getMessage(), Project::MSG_ERR);
-                    }
-                }
-
-                // get a dynamic filename which we will output at the end
-                $unexpanded = str_replace($partner, '`$partnerName`', $set);
-            }
+            $latest = $this->_build($bundlefile, $files);
+        }
+        catch (Exception $e)
+        {
+            throw new BuildException($e->getMessage());
         }
 
-        if (strpos($unexpanded, '$partnerName') !== false)
-        {
-            // output the dynamic CSS call - on page load, Smarty will find the 
-            // final version
-            $webRoot = $this->_webRoot("$unexpanded-*.css");
-            $output .= "<link href=\"$webRoot/{glob file=\"$unexpanded-*.css\"}\" rel=\"stylesheet\" type=\"text/css\" />\n";
-        }
-        else
-        {
-            // output the static CSS tag
-            $webRoot = $this->_webRoot("$set-$latest.css");
-            $output .= "<link href=\"$webRoot/$set-$latest.css\" rel=\"stylesheet\" type=\"text/css\" />\n";
-        }
-
+        // output the static CSS tag
+        $twoBitValue = 0x3 & crc32("$bundlefile-$latest.css");
+        $host = str_replace('?', $twoBitValue, $this->_host);
+        $path = "http://$host/$bundlefile-$latest.css";
+        $output = "<link href=\"$path\" rel=\"stylesheet\" type=\"text/css\" />\n";
         return $output;
     }
 
-    private function _webRoot($file)
+    private function _build($bundlefile, $files)
     {
-        $twoBitValue = 0x3 & crc32($file);
-        return str_replace("?", $twoBitValue, $this->_staticWebRoot);
-    }
+        static $alreadyLoaded = array();
 
-    private function _determineSet($urls, &$latest, $partner)
-    {
-        // determine correct versions and latest mod time of files
-        // return the name of the combined set of files
-
-        $files = array();
+        $buffer = '';
         $latest = 0;
-        foreach ($urls as $file)
-        {
-            $file = str_replace('$partnerName', $partner, $file);
-            if ( $partner != 'core' && ($mtime = @filemtime( $this->_cssDir . "/partners/$partner$file")) !== false )
-            {
-                $files[] = "/partners/$partner$file";
-            }
-            else if ( ($mtime = @filemtime( $this->_cssDir . $file)) !== false )
-            {
-                $files[] = $file;
-            }
 
-            if ($mtime !== false && $mtime > $latest)
-            {
-                $latest = $mtime;
-            }
-        }
-
-        // if no files are found
-        if (empty($files))
-        {
-            return false;
-        }
-
-        return $files;
-    }
-
-    private function _compareSet($set, $latest)
-    {
-        $existing = glob($this->_cssDir . "/$set-*.css");
-
-        // if the set does not exist, create new set outright
-        if (! $existing[0])
-        {
-            return $latest;
-        }
-
-        // else the set already exists
-        else
-        {
-            // get the existing set's latest mod time (in the filename)
-            preg_match("/-([0-9]+)\./", $existing[0], $matches);
-            $oldLatest = $matches[1];
-
-            // compare the latest mod time of the set to the existing file name
-            // if $latest is greater than $oldLatest, create new file
-            if ($latest > $oldLatest)
-            {
-                // delete the old file
-                @unlink($this->_cssDir . "/$set-$oldLatest.css");
-
-                return $latest;
-            }
-        }
-
-        // else source files have not changed - no need to build bundle
-        return false;
-    }
-
-    private function _saveSet($set, $files, $timestamp)
-    {
-        // build the output file
-        if (($handle = fopen($this->_cssDir . "/$set-$timestamp.css", 'w')) === false)
-        {
-            throw new Exception("Unable to create {$this->_cssDir}/$set-$timestamp.css");
-        }
-
-        // combine all files into one
         foreach ($files as $file)
         {
-            $cssSrcFile = $this->_cssDir . $file;
-            
-            // compress file contents
-            $contents = trim(YuiCompress::compress ($cssSrcFile, YuiCompress::CSS_TYPE));
-            
-            if (fwrite($handle, $contents) === false)
+            if (isset($alreadyLoaded[$file]))
             {
-                fclose($handle);
-                throw new Exception("Unable to write to {$this->_cssDir}/$set-$timestamp.css");
+                continue;
             }
+            else
+            {
+                $alreadyLoaded[$file] = true;
+            }
+
+            $file = trim($file, '/');
+
+            $mtime = filemtime("{$this->_webRoot}/$file");
+            if ($mtime > $latest)
+            {
+                // get the latest modification time
+                $latest = $mtime;
+            }
+
+            $buffer .= $this->_minify("{$this->_webRoot}/$file") . "\n";
+        }
+
+        // open the output file
+        if (! file_exists($this->_toDir) && ! mkdir($this->_toDir, 0755, true))
+        {
+            throw new Exception("Unable to create {$this->_toDir}");
+        }
+        
+        if (($handle = fopen("{$this->_toDir}/$bundlefile-$latest.css", 'w')) === false)
+        {
+            throw new Exception("Unable to create {$this->_toDir}/$bundlefile-$latest.css");
+        }
+
+        if (fwrite($handle, $buffer) === false)
+        {
+            fclose($handle);
+            throw new Exception("Unable to write to {$this->_toDir}/$bundlefile-$latest.css");
         }
 
         fclose($handle);
+
+        return $latest;
+    }
+
+    private function _minify($file)
+    {
+        $this->_phing->log("Attempting to minify $file", Project::MSG_VERBOSE);
+
+        $tmp = (function_exists('sys_get_temp_dir')) ? sys_get_temp_dir() : '/tmp';
+        $tmpFile = tempnam($tmp, "yui");
+
+        $command = "java -jar " . $this->_yuiPath . " --type css -o $tmpFile $file";
+        exec($command, $dummy, $status);
+
+        if ($status === 0)
+        {
+            $buffer = file_get_contents($tmpFile);
+        }
+        else
+        {
+            // if error, return original file
+            $buffer = file_get_contents($file);
+        }
+
+        @unlink($tmpFile);
+        return $buffer;
     }
 }
 
---
+++ phing/filters/OptimizeCSS.php
@@ -0,0 +1,102 @@
+<?php
+
+require_once 'phing/filters/BaseParamFilterReader.php';
+include_once 'phing/filters/ChainableReader.php';
+require_once 'phing/filters/CSSOptimizer.php';
+
+class OptimizeCSS extends BaseParamFilterReader implements ChainableReader
+{
+    /**
+     * Returns the filtered stream. 
+     * The original stream is first read in fully, then the HTML is updated
+     * for this partner.
+     * 
+     * @param int $len Required $len for Reader compliance.
+     * 
+     * @return mixed The filtered stream, or -1 if the end of the resulting stream has been reached.
+     * 
+     * @exception IOException if the underlying stream throws an IOException
+     * during reading
+     */
+    function read($len = null)
+    {
+        // read from next filter up the chain
+        $buffer = $this->in->read($len);
+
+        if ($buffer === -1)
+        {
+            return -1;
+        }
+
+        $host = $webRoot = $toDir = $yuiPath = '';
+        $params = $this->getParameters();
+
+        // collect parameters.
+        foreach ($params as $param)
+        {
+            //echo $param->getName() . ': ' . $param->getValue() . "\n";
+            if ($param->getName() == "static.host")
+            {
+                $host = $param->getValue();
+            }
+            else if ($param->getName() == "webroot")
+            {
+                $webRoot = $param->getValue();
+            }
+            else if ($param->getName() == "todir")
+            {
+                $toDir = $param->getValue();
+            }
+            else if ($param->getName() == "yui.path")
+            {
+                $yuiPath = $param->getValue();
+            }
+        }
+
+        if (empty($host))
+        {
+            throw new BuildException("\"static.host\" parameter not set");
+        }
+        else if (empty($webRoot))
+        {
+            throw new BuildException("\"webroot\" parameter not set");
+        }
+        else if (empty($toDir))
+        {
+            throw new BuildException("\"todir\" parameter not set");
+        }
+        else if (empty($yuiPath))
+        {
+            throw new BuildException("\"yui.path\" parameter not set");
+        }
+
+        // find groups of CSS link tags and replace them with a single link a
+        // bundled, minified, timestamped resource. 
+
+        $optomizer = new CSSOptimizer($host, $yuiPath, $webRoot, $toDir, $this);
+        $pattern = '/(<link .*href=".*".*\/>\s*)+/';
+        $buffer = preg_replace_callback($pattern, array($optomizer, "optimize"), $buffer);
+
+
+        return $buffer;
+    }
+
+    /**
+     * Creates a new OptimizeCSS filter using the passed in Reader for
+     * instantiation.
+     * 
+     * @param Reader $reader A Reader object providing the underlying stream.
+     *               Must not be <code>null</code>.
+     * 
+     * @return OptimizeCSS A new filter based on this configuration, but filtering
+     *         the specified reader
+     */
+    function chain(Reader $reader)
+    {
+        $newFilter = new OptimizeCSS($reader);
+        $newFilter->setProject($this->getProject());
+        return $newFilter;
+    }
+}
+
+?>
